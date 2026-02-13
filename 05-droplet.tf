253b76aab6411663790f1b4412abe0faf2a65215
Andrew Siwko <asiwko@siwko.org>
2026-02-13 15:35:44 -0500

    update droplet

--- 05-droplet.tf
+++ 05-droplet.tf
@@ -1,8 +1,10 @@
-# resource "digitalocean_droplet" "vm01" {
-#  image = "ubuntu-22-04-x64"
-#  name = "web-server"
-#  region = "nyc3"
-#  size = "s-1vcpu-1gb"
+resource "digitalocean_droplet" "vm01" {
+ image = local.rocky_image.id
+ name = "vm01"
+ region = var.instance_region
+ size = local.cheapest_size.slug
 #  ssh_keys = [data.digitalocean_ssh_key.terraform.id]
-
-# }
+}
+output "droplet_ip" {
+  value = digitalocean_droplet.vm01.ipv4_address
+}   

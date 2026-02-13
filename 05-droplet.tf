a80559d677d677dd5dfe8cfd9fe3ecce4843c123
Andrew Siwko <asiwko@siwko.org>
2026-02-13 17:03:46 -0500

    add an ssh key to the vm

--- 05-droplet.tf
+++ 05-droplet.tf
@@ -1,9 +1,14 @@
+resource "digitalocean_ssh_key" "ansible_key" {
+  name       = "ansible-key"
+  public_key = file("/container_shared/ansible/ansible_rsa.pub")
+}
+
 resource "digitalocean_droplet" "vm01" {
  image = local.rocky_image.id
  name = "vm01"
  region = var.instance_region
  size = local.cheapest_size.slug
-#  ssh_keys = [data.digitalocean_ssh_key.terraform.id]
+ ssh_keys = [data.digitalocean_ssh_key.ansible_key.id]
 }
 output "droplet_ip" {
   value = digitalocean_droplet.vm01.ipv4_address

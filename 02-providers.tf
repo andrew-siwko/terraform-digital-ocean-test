807171c610f1a509f79dd4a6d7cc509a07c5a827
Andrew Siwko <asiwko@siwko.org>
2026-02-13 14:24:56 -0500

    local back end; formatting

--- 02-providers.tf
+++ 02-providers.tf
@@ -1,21 +1,25 @@
 terraform {
- required_providers {
-   digitalocean = {
-     source = "digitalocean/digitalocean"
-     version = "~> 2.0"
-   }
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
     # in order to update DNS on linode, we'll need the linode provider.
     linode = {
-      source  = "linode/linode"
+      source = "linode/linode"
     }
- }
+  }
+  # this is temporary until we configure a remote back end.
+  backend "local" {
+    path = "terraform.tfstate"
+  }
 }
 
 provider "digitalocean" {
- token = var.DIGITAL_OCEAN_TOKEN
+  token = var.DIGITAL_OCEAN_TOKEN
 }
 data "digitalocean_ssh_key" "terraform" {
- name = "your_ssh_key_name"
+  name = "your_ssh_key_name"
 }
 
 

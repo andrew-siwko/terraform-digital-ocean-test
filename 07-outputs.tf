3880515a822f36581f233c7a7d722813fa263928
Andrew Siwko <asiwko@siwko.org>
2026-02-13 14:12:48 -0500

    comment out creation to get project building

--- 07-outputs.tf
+++ 07-outputs.tf
@@ -1,3 +1,3 @@
-output "digital_ocean_instance_public_ip" {
-  value = azurerm_linux_virtual_machine.vm_01.public_ip_address
-}
+# output "digital_ocean_instance_public_ip" {
+#   value = azurerm_linux_virtual_machine.vm_01.public_ip_address
+# }
